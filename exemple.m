5c571ac3a9722592d4cfed135f1154447248a4b4
Paulo de Souza <paulohbs2001@gmail.com>
2023-11-14 20:23:48 -0300

    nada

--- exemple.m
+++ exemple.m
@@ -2,16 +2,20 @@ close all
 clear
 clc
 %%
-folder1 = '/Paulo (bolsista)/Data_Analyzer/data/Logger_formula_1';
-folder2 = '/Paulo (bolsista)/Data_Analyzer/data/Logger_formula_2';
-folder3 = '/Paulo (bolsista)/Data_Analyzer/data/Rollout_teste_cruze';
+folder1 = "C:\Users\paulo\Documents\MATLAB\Data-Analyzer\data\Logger_formula_1";
+folder2 = "C:\Users\paulo\Documents\MATLAB\Data-Analyzer\data\Logger_formula_2";
+folder3 = "C:\Users\paulo\Documents\MATLAB\Data-Analyzer\data\Rollout_teste_cruze";
+%folder1 = '/Paulo (bolsista)/Data_Analyzer/data/Logger_formula_1';
+%folder2 = '/Paulo (bolsista)/Data_Analyzer/data/Logger_formula_2';
+%folder3 = '/Paulo (bolsista)/Data_Analyzer/data/Rollout_teste_cruze';
 sensor = "Accelerometer";
 Aq = 100; % Hz
-%%
-data = AnalyzeClass(folder3, sensor, Aq);
+%% Parâmetros
+data = AnalyzeClass(folder2, sensor, Aq);
 CG = [0.9 0.0 0.25];                
 L = 1.55;   
 g = 9.81;
+W = 0;
 %% Acquisition rate [Hz]
 figure(1)
 plot(data.acqrt)
