df9b96cc390709d9ebab0ec46b31dbf6158cb54c
Eduardo Brum <eduardoobs2@gmail.com>
2023-11-23 14:50:27 -0300

    [ADD] changed startup file to a better function named AddLib

---
+++ AddLib.m
@@ -0,0 +1,18 @@
+function AddLib(baseFolder)
+if nargin == 0
+    baseFolder = fileparts( which( mfilename ) );
+end
+
+addpath(baseFolder);
+
+addingfolders = dir( baseFolder );
+for folderid = 3 : size( addingfolders, 1 )
+    if addingfolders(folderid).isdir
+        if addingfolders(folderid).name(1) == '.' || addingfolders(folderid).name(1) == '+' || addingfolders(folderid).name(1) == '@'
+            %.gitignore and .git folder
+        else
+            AddLib([ baseFolder, filesep, addingfolders(folderid).name])
+        end
+    end
+end
+savepath
--- startup.m
+++
@@ -1,7 +0,0 @@
-% Substitua 'caminho/pasta1', 'caminho/pasta2', etc., pelos caminhos reais das suas pastas
-pastas = {'D:\Paulo-bolsista\Data-Analyzer\Analyzer', 'D:\Paulo-bolsista\Data-Analyzer\data', 'D:\Paulo-bolsista\Data-Analyzer\Data-Analyzer'};
-
-% Loop para adicionar cada pasta ao caminho
-for i = 1:length(pastas)
-    addpath(genpath(pastas{i}));
-end
