06b3c2bda8e5c5fe6e30298664551ee8474d951d
Paulo de Souza <paulohbs2001@gmail.com>
2024-02-17 16:51:05 -0300

    [BUGFIX] CG correction fixed

--- @AccClass/AccClass.m
+++ @AccClass/AccClass.m
@@ -12,9 +12,6 @@ classdef AccClass < handle
                 case "Phone"
                     obj.read = ReadClass(folder, "Accelerometer");
                     obj.gyr = GyroscopeClass(folder);
-                    obj.read.fft()
-                    obj.read.filter(5)
-                    obj.read.scale(1/9.81)
                     
                 case "PIG"
                     obj.read = ReadClass(folder, "PIG");              
@@ -24,9 +21,12 @@ classdef AccClass < handle
         function correctCG(obj)
             switch obj.logger
                 case "Phone"
-                    obj.read.data(:, 1) = obj.read.data(:, 1) + obj.gyr.w.*(obj.gyr.w*obj.C(1)) + obj.gyr.alpha.*obj.C(1);
-                    obj.read.data(:, 2) = obj.read.data(:, 2) + obj.gyr.w.*(obj.gyr.w*obj.C(2)) + obj.gyr.alpha.*obj.C(2);
-                    obj.read.data(:, 3) = obj.read.data(:, 3) + obj.gyr.w.*(obj.gyr.w*obj.C(3)) + obj.gyr.alpha.*obj.C(3);
+                    index = (size(obj.read.data, 1) - size(obj.gyr.w, 1)) + 1;
+                    obj.read.data = obj.read.data(index:end, :);
+                    obj.read.t = obj.read.t(:, index:end);
+                    obj.read.data(:, 1) = obj.read.data(:, 1) + obj.gyr.w.*(obj.gyr.w*obj.C(1)) + obj.gyr.alpha.*obj.C(1); % z
+                    obj.read.data(:, 2) = obj.read.data(:, 2) + obj.gyr.w.*(obj.gyr.w*obj.C(2)) + obj.gyr.alpha.*obj.C(2); % y
+                    obj.read.data(:, 3) = obj.read.data(:, 3) + obj.gyr.w.*(obj.gyr.w*obj.C(3)) + obj.gyr.alpha.*obj.C(3); % x
             end
         end
     end
