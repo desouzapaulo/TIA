f0a3b84a8ea3dbea72f90fa46cc5b22b36d9b69e
Paulo de Souza <paulohbs2001@gmail.com>
2024-06-12 23:36:11 -0300

    [MOD] now parameters

--- example.m
+++ example.m
@@ -10,9 +10,10 @@ filename = folder + "\parameters\RS11.xlsx";
 parameters = readmatrix(filename);
 mup = 0.45; % coefficient of friction of the brake pad
 IW = [1.505 1.505]; % moment of inercia of the wheel (front, rear) [m^4***]
-BBB = 0.37; % Brake balance bar proportion (percentage of braking balance to the rear)
-psi = 0.54;
-chi = 0.3;
+
+BBB = 0.5; % Brake balance bar proportion (percentage of braking balance to the rear)
+psi = 0.5;
+chi = 0.2;
 
 % ======================================OUTPUT================================================
 %% Initial
@@ -22,8 +23,5 @@ brake = BrakeClass(foldername, logger, psi, chi, BBB, parameters);
 brake.calcOptBrake
 brake.calcRealBrake
 brake.calcCntFriction
-% fprintf('\nHydraulic pressure: %.3f', brake.Pl)
-% fprintf('\nBraking force : %.3f [N]',brake.Fx_real)
-% fprintf('\nBraque dist: %.2f',brake.phi)
 %% Plots
 brake.pltbrakeline
